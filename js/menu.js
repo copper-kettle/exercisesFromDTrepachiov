565a3425ea93077e324636e91df61e41aae0a152
copper-kettle <107906619+copper-kettle@users.noreply.github.com>
2024-04-14 10:44:45 +0300

    dtr_20240414

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,9 @@
 let level = document.querySelectorAll(".menu_level");
-let level_items = document.querySelector(".menu_level_items");
+// let level_items = document.querySelector(".menu_level_items");
 let menu_stage = document.querySelector('.menu_stage');
-console.log(level);
 
 for (let i = 0; i < level.length; i++) {
-	level[i].addEventListener("click", function() {
-    level_items.classList.toggle('hide');
-}
-);
-	
+	level[i].addEventListener('click', function () {
+		level[i].querySelector(".menu_level_items").classList.toggle('hide');
+	});
 }
